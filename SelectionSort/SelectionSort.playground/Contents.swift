34f57f48e966917e8f87ed51c95b61bd76a5ff6a
Kumar Singh, Randhir <rkunv@allstate.com>
2021-04-05 22:22:48 +0530

    made selectionsort generic

--- SelectionSort/SelectionSort.playground/Contents.swift
+++ SelectionSort/SelectionSort.playground/Contents.swift
@@ -2,7 +2,7 @@
 var str = "Hello, playground"
 
 struct SelectionSort {
-    static func sort(array: inout [Int]) -> [Int]{
+    static func sort<T: Comparable>(array: inout [T]) -> [T]{
         for i in 0...array.count - 2 {
             var minmIndex = i
             
@@ -20,7 +20,9 @@ struct SelectionSort {
 }
 
 
-var array = [4,9,5,6,3,1,7,8,2,14,11,13,12,18,15,19,16,20,17]
-
-SelectionSort.sort(array: &array)
-print(array)
+var array1 = [4,9,5,6,3,1,7,8,2,14,11,13,12,18,15,19,16,20,17]
+var array2 = ["zebra","hello", "apple", "john", "flower", "martin", "mango", "man", "manima"]
+SelectionSort.sort(array: &array1)
+print(array1)
+SelectionSort.sort(array: &array2)
+print(array2)
