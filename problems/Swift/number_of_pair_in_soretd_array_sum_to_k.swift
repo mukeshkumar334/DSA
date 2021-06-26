20866f5d56df8c3164fbab56f81d25e6c77478a9
Randhir Singh <srandhir.334@gmail.com>
2021-06-26 10:45:00 +0530

    Create number_of_pair_in_soretd_array_sum_to_k.swift

---
+++ problems/Swift/number_of_pair_in_soretd_array_sum_to_k.swift
@@ -0,0 +1,34 @@
+//Given a sorted array, find the number of pairs which sums to K.
+func pairCount(in array: [Int], k: Int) -> Int { 
+    var i=0, j = array.count-1
+    var pairCount = 0
+    while i < j {
+        if array[i] + array[j] > k {
+            j -= 1
+        } else if array[i] + array[j] < k {
+            i += 1
+        } else {
+            if array[i] != array[j] { 
+                var prevI = i
+                var prevJ = j
+                while array[i+1] == array[prevI] {
+                    i += 1
+                }
+                
+                while array[j-1] == array[prevJ] {
+                    j -= 1
+                }
+                pairCount += (i-prevI+1)*(prevJ-j+1)
+                i += 1
+                j -= 1
+            } else {
+                pairCount += (j-i)
+                break
+            }
+        }
+    }
+    return pairCount
+}
+//Test
+let array = [1,4,4,5,5,5,6,6,11]
+print(pairCount(in: array, k: 11))
