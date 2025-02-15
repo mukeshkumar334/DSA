076143eabcf2b525aa4581c3b8eacbbc6f9cc2bb
mukesh <mk41206@gmail.com>
2025-02-15 16:20:04 +0530

    SCRUM-4 added how are you message

--- data_structures/Java/BinaryTree.java
+++ data_structures/Java/BinaryTree.java
@@ -1,6 +1,6 @@
 class BinaryTree {
     public static void main(String[] args) {
         System.out.println("welcome !");
-        System.out.println("helloWorld!");
+        System.out.println("helloWorld! ,how are you!");
     }
 }
